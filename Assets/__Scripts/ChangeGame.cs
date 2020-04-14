4fa0167cbf08ee0bc46b151c2c77648fea2be0d3
Shirin <g00363458@gmit.ie>
2020-04-14 10:57:18 +0100

    changed level name

---
+++ Assets/__Scripts/ChangeGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+/*class to control the timer in the game scene.
+ Timer then continues onto level 2 once time count ends */
+
+public class ChangeGame : MonoBehaviour
+{
+    // Variable to hold what level to continue to
+    public string levelToLoad;
+    // Timer variable
+    private float timer = 20f;
+    // Timer text on screen
+    private Text timerSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start the timer 
+        timerSeconds = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        // Converting timer to text
+        timerSeconds.text = timer.ToString("f0");
+
+        /* When timer is less than or equal to 0
+        then load next level 
+        */
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
